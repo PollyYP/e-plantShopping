34f6239cc84e94918b74415d1207c0ba5cdbd916
Polly Yospan <porgroovy@gmail.com>
2024-09-07 11:47:39 -0400

    implement handleAddToCart function

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect, useDispatch } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -238,20 +241,28 @@ function ProductList() {
     }
 
     const handleCartClick = (e) => {
-    e.preventDefault();
-    setShowCart(true); // Set showCart to true when cart icon is clicked
+        e.preventDefault();
+        setShowCart(true); // Set showCart to true when cart icon is clicked
     };
 
     const handlePlantsClick = (e) => {
-    e.preventDefault();
-    setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
-    setShowCart(false); // Hide the cart when navigating to About Us
+        e.preventDefault();
+        setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
+        setShowCart(false); // Hide the cart when navigating to About Us
     };
 
     const handleContinueShopping = (e) => {
-    e.preventDefault();
-    setShowCart(false);
+        e.preventDefault();
+        setShowCart(false);
     };
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+           ...prevState,
+           [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+         }));
+      };
 
     return (
         <div>
