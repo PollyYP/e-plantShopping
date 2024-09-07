561ff51e9ec6c4ac7af2e5e9eb4338da680e0861
Polly Yospan <porgroovy@gmail.com>
2024-09-07 18:39:14 -0400

    adjust style in ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -312,7 +312,11 @@ function ProductList() {
         <div className="product-grid">
             {plantsArray.map((category, index) => (
             <div key={index}>
-                <h1><div>{category.category}</div></h1>
+                <h1 style={{ textAlign: 'center' }}>
+                    <div className="product-category">
+                        {category.category}
+                    </div>
+                </h1>
                 <div className="product-list">
                     {category.plants.map((plant, plantIndex) => (
                     <div className="product-card" key={plantIndex}>
